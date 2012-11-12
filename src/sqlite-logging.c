164d572729022f0f6486659fb4a4824ad361bc64
ejrh <ejrh00@gmail.com>
2012-11-12 14:02:04 +1300

    Reuse statement object for later insertions.

--- src/sqlite-logging.c
+++ src/sqlite-logging.c
@@ -11,6 +11,7 @@ typedef struct LogContext
 {
     char *filename;
     sqlite3 *conn;
+    sqlite3_stmt *insert_stmt;
 } LogContext;
 
 static void create_log_table(LogContext *context)
@@ -43,6 +44,7 @@ LogContext *create_sqlite_context(char *filename)
         free(context);
         return NULL;
     }
+    context->insert_stmt = NULL;
     
     create_log_table(context);
     
@@ -51,30 +53,41 @@ LogContext *create_sqlite_context(char *filename)
 
 void sqlite_log(LogContext *context, char *host, char *timestamp, char *oid, char *value)
 {
-    sqlite3_stmt *stmt;
-    
-    if (sqlite3_prepare_v2(context->conn, INSERT_INTO_LOG_SQL, sizeof(INSERT_INTO_LOG_SQL), &stmt, NULL) != SQLITE_OK)
+    if (context->insert_stmt == NULL && sqlite3_prepare_v2(context->conn, INSERT_INTO_LOG_SQL, sizeof(INSERT_INTO_LOG_SQL), &context->insert_stmt, NULL) != SQLITE_OK)
     {
         fprintf(stderr, "%s", sqlite3_errmsg(context->conn));
         abort();
     }
     
-    sqlite3_bind_text(stmt, 1, host, -1, SQLITE_TRANSIENT);
-    sqlite3_bind_text(stmt, 2, timestamp, -1, SQLITE_TRANSIENT);
-    sqlite3_bind_text(stmt, 3, oid, -1, SQLITE_TRANSIENT);
-    sqlite3_bind_text(stmt, 4, value, -1, SQLITE_TRANSIENT);
+    sqlite3_bind_text(context->insert_stmt, 1, host, -1, SQLITE_TRANSIENT);
+    sqlite3_bind_text(context->insert_stmt, 2, timestamp, -1, SQLITE_TRANSIENT);
+    sqlite3_bind_text(context->insert_stmt, 3, oid, -1, SQLITE_TRANSIENT);
+    sqlite3_bind_text(context->insert_stmt, 4, value, -1, SQLITE_TRANSIENT);
     
-    if (sqlite3_step(stmt) != SQLITE_DONE)
+    if (sqlite3_step(context->insert_stmt) != SQLITE_DONE)
     {
         fprintf(stderr, "%s", sqlite3_errmsg(context->conn));
         abort();
     }
     
-    sqlite3_finalize(stmt);
+    if (sqlite3_reset(context->insert_stmt) != SQLITE_OK)
+    {
+        fprintf(stderr, "%s", sqlite3_errmsg(context->conn));
+        abort();
+    }
+    
+    if (sqlite3_clear_bindings(context->insert_stmt) != SQLITE_OK)
+    {
+        fprintf(stderr, "%s", sqlite3_errmsg(context->conn));
+        abort();
+    }
 }
 
 void destroy_sqlite_context(LogContext *context)
 {
+    if (context->insert_stmt)
+        sqlite3_finalize(context->insert_stmt);
+
     sqlite3_close(context->conn);
     free(context);
 }
